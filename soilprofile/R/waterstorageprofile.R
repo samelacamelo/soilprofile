2a4e7c97c332b03f5a145ec002d81f00ea790d7e
rroman12 <rroman12@its.jnj.com>
2022-10-20 12:10:36 -0300

    started working with plots

--- soilprofile/R/waterstorageprofile.R
+++ soilprofile/R/waterstorageprofile.R
@@ -8,12 +8,26 @@
 
 
 waterstorageprofile <- function(){
-  require(zoo)
-  filePath <- file.choose()
+  library(ggplot2)
+  #filePath <- file.choose()
+  filePath <- "C:\\Users\\rroman12\\Downloads\\unicalinha.csv"
   df <- read.csv(file = filePath,header = TRUE)
 
   df2 <- df[, colnames(df)[c(3:ncol(df))]]
-  #edit(df2)
-  plot(df2)
+
+  df3 <- destroyX(df2)
+  #edit(df3)
+  ggplot(df3, aes(x=df3$x, y=df3$x))
   #plot(df2$y, df2$x)
+}
+
+
+destroyX = function(es) {
+  f = es
+  for (col in c(1:ncol(f))){ #for each column in dataframe
+    if (startsWith(colnames(f)[col], "X") == TRUE)  { #if starts with 'X' ..
+      colnames(f)[col] <- substr(colnames(f)[col], 2, 100) #get rid of it
+    }
+  }
+  assign(deparse(substitute(es)), f, inherits = TRUE) #assign corrected data to original name
 }
